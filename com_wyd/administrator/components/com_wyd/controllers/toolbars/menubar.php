1208d5e9d8de43579d4024b31aa813fbee2fb363
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-03 10:05:18 -0400

    Added component wide submenubar

---
+++ com_wyd/administrator/components/com_wyd/controllers/toolbars/menubar.php
@@ -0,0 +1,33 @@
+<?
+
+class ComWYDControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
+{
+	
+	public function getCommands()
+	{
+		
+		$name = $this->getController()->getIdentifier()->name;
+		
+		$this->addCommand('Overview', array(
+			'href'	=> 'index.php?option=com_wyd&view=dashboard',
+			'active'	=> ($name == 'dashboard'),
+		));
+		
+		$this->addCommand('Days', array(
+			'href'	=> 'index.php?option=com_wyd&view=days',
+			'active'	=> ($name == 'day'),
+		));
+		
+		$this->addCommand('Sponsorships', array(
+			'href'	=> 'index.php?option=com_wyd&view=sponsorships',
+			'active'	=> ($name == 'sponsorship'),
+		));
+		
+		$this->addCommand('Reports', array(
+			'href'	=> 'index.php?option=com_wyd&view=reports',
+			'active'	=> ($name == 'report'),
+		));
+		
+		return parent::getCommands();
+	}
+}
