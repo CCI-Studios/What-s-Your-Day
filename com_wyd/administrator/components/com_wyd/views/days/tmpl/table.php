2606ce756690b14864be8ec2a5c5b34f111c5ed4
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-04 21:50:57 -0400

    Changed content of view and made links modal

--- com_wyd/administrator/components/com_wyd/views/days/tmpl/table.php
+++ com_wyd/administrator/components/com_wyd/views/days/tmpl/table.php
@@ -5,9 +5,8 @@
 	<? foreach ($days as $day): ?>
 	<tr>
 		<td class="key"><?= $day->title ?></td>
-		<td><?= $day->date ?></td>
-		<td><a href="<?= @route('view=day&amp;id='. $day->id) ?>" class="modal" rel="{handler: iframe}">
-			->
+		<td><a href="<?= @route('view=day&amp;id='. $day->id .'&amp;tmpl=component') ?>" class="modal" rel="{handler: 'iframe'}">
+			<?= date('D, F jS, Y', strtotime($day->date)) ?>
 		</a></td>
 	</tr>
 	<? endforeach; ?>
